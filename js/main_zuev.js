e9e72016e2ea1eefe622b627a1c8f76494272af3
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-09-17 14:55:11 +0500

    end

--- js/main_zuev.js
+++ js/main_zuev.js
@@ -1,25 +1,30 @@
-// const cardProductsList = document.querySelector('.main-sell-slide-list');
-// const cardProducts = cardProductsList.children;
-// console.log(cardProducts);
+// let cards = document.querySelectorAll('.main-sell-cart');
 //
-// const tableHaractreristics = document.querySelectorAll('.body-row-tablet-yacheika');
+// let yacheikas = document.querySelectorAll('.body-row-tablet-yacheika');
+// let yacheikasTitle = document.querySelectorAll('.header-row-table-yacheika');
+// console.log(yacheikasTitle);
 //
-// cardProducts[0].addEventListener('mouseover', (e) => {
-//     console.log('Ghbfdth');
-//     tableHaractreristics[0].style.backgroundColor = 'green';
-// })
-// body-row-tablet-yacheika
-
-let cards = document.querySelectorAll('.main-sell-cart');
-// let card = cards.children;
-
-let yacheikas = document.querySelectorAll('.body-row-tablet-yacheika');
-
-for (let cardItem of cards) {
-    cardItem.addEventListener('mouseover', () => {
-        console.log('jfkl');
-        for (let yacheika of yacheikas) {
-            yacheika.classList.toggle('body-row-tablet-yacheika-sel');
-        }
-    })
-}
+// for (let cardItem of cards) {
+//     cardItem.addEventListener('click', () => {
+//         console.log('jfkl');
+//         for (let yacheika of yacheikas) {
+//             let cl = yacheika.classList.contains('body-row-tablet-yacheika-sel');
+//             if (!cl) {
+//                 yacheika.classList.add('body-row-tablet-yacheika-sel');
+//             } else {
+//                 yacheika.classList.remove('body-row-tablet-yacheika-sel');
+//             }
+//
+//         }
+//
+//         for (let yacheikaTitle of yacheikasTitle) {
+//             let cl = yacheikaTitle.classList.contains('body-row-tablet-yacheika-sel');
+//             if (!cl) {
+//                 yacheikaTitle.classList.add('body-row-tablet-yacheika-sel');
+//             } else {
+//                 yacheikaTitle.classList.remove('body-row-tablet-yacheika-sel');
+//             }
+//             // yacheikaTitle.classList.toggle('body-row-tablet-yacheika-sel');
+//         }
+//     })
+// }
