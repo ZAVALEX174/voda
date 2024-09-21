c3080084db6aa2d20e83c696a8bf1765df8abbc2
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-09-21 13:26:18 +0500

    fixed style css .tables-box-row:last-child .body-row:last-child .body-row-tablet .body-row-tablet-yacheika

--- js/main_zuev.js
+++ js/main_zuev.js
@@ -29,84 +29,87 @@
 //     })
 // }
 
-// let cart1 = document.querySelector('[data-js-cart-item1]');
-// let cart2 = document.querySelector('[data-js-cart-item2]');
-// let cart3 = document.querySelector('[data-js-cart-item3]');
-// let cart4 = document.querySelector('[data-js-cart-item4]');
-let carts = document.querySelectorAll('[data-js-cart-item]');
+let cart1 = document.querySelector('[data-js-cart-item1]');
+let cart2 = document.querySelector('[data-js-cart-item2]');
+let cart3 = document.querySelector('[data-js-cart-item3]');
+let cart4 = document.querySelector('[data-js-cart-item4]');
+
 
 let tables1 = document.querySelectorAll('[data-js-table-yach1]');
 let tables2 = document.querySelectorAll('[data-js-table-yach2]');
 let tables3 = document.querySelectorAll('[data-js-table-yach3]');
 let tables4 = document.querySelectorAll('[data-js-table-yach4]');
-carts.forEach(cart => {
-    cart.addEventListener('mouseover', (e) => {
-        e.preventDefault();
-        tables1.forEach((table1)=>{
-            table1.classList.add('body-row-tablet-yacheika-sel')
-        })
-    })
 
+//Дорабоать======================================
+// let carts = document.querySelectorAll('[data-js-cart-item]');
+// carts.forEach(cart => {
+//     cart.addEventListener('mouseover', (e) => {
+//         e.preventDefault();
+//         tables1.forEach((table1)=>{
+//             table1.classList.add('body-row-tablet-yacheika-sel')
+//         })
+//     })
+//
+// })
+// carts.forEach(cart => {
+//     cart.addEventListener('mouseout', (e) => {
+//         e.preventDefault();
+//         tables1.forEach((table1)=>{
+//             table1.classList.remove('body-row-tablet-yacheika-sel')
+//         })
+//     })
+// })
+//============================================
+//
+cart1.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    tables1.forEach((table1) => {
+        // table1.style.backgroundColor = "rgba(10, 165, 240, 0.1)";
+        table1.classList.add('body-row-tablet-yacheika-sel')
+    });
 })
-carts.forEach(cart => {
-    cart.addEventListener('mouseout', (e) => {
-        e.preventDefault();
-        tables1.forEach((table1)=>{
-            table1.classList.remove('body-row-tablet-yacheika-sel')
-        })
-    })
+cart1.addEventListener("mouseout", function (event) {
+    event.preventDefault();
+    tables1.forEach((table1) => {
+        table1.classList.remove('body-row-tablet-yacheika-sel')
+    });
 })
 
-//
-// cart1.addEventListener("mouseover", function (event) {
-//     event.preventDefault();
-//     tables1.forEach((table1) => {
-//         // table1.style.backgroundColor = "rgba(10, 165, 240, 0.1)";
-//         table1.classList.add('body-row-tablet-yacheika-sel')
-//     });
-// })
-// cart1.addEventListener("mouseout", function (event) {
-//     event.preventDefault();
-//     tables1.forEach((table1) => {
-//         table1.classList.remove('body-row-tablet-yacheika-sel')
-//     });
-// })
-//
-// cart2.addEventListener("mouseover", function (event) {
-//     event.preventDefault();
-//     tables2.forEach((table2) => {
-//         table2.classList.add('body-row-tablet-yacheika-sel')
-//     });
-// })
-// cart2.addEventListener("mouseout", function (event) {
-//     event.preventDefault();
-//     tables2.forEach((table2) => {
-//         table2.classList.remove('body-row-tablet-yacheika-sel')
-//     });
-// })
-//
-// cart3.addEventListener("mouseover", function (event) {
-//     event.preventDefault();
-//     tables3.forEach((table3) => {
-//         table3.classList.add('body-row-tablet-yacheika-sel')
-//     });
-// })
-// cart3.addEventListener("mouseout", function (event) {
-//     event.preventDefault();
-//     tables3.forEach((table3) => {
-//         table3.classList.remove('body-row-tablet-yacheika-sel')
-//     });
-// })
-//
-// cart4.addEventListener("mouseover", function (event) {
-//     event.preventDefault();
-//     tables4.forEach((table4) => {
-//         table4.classList.add('body-row-tablet-yacheika-sel')
-//     });
-// })
-// cart4.addEventListener("mouseout", function (event) {
-//     event.preventDefault();
-//     tables4.forEach((table4) => {
-//         table4.classList.remove('body-row-tablet-yacheika-sel')
-//     });
-// })
+cart2.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    tables2.forEach((table2) => {
+        table2.classList.add('body-row-tablet-yacheika-sel')
+    });
+})
+cart2.addEventListener("mouseout", function (event) {
+    event.preventDefault();
+    tables2.forEach((table2) => {
+        table2.classList.remove('body-row-tablet-yacheika-sel')
+    });
+})
+
+cart3.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    tables3.forEach((table3) => {
+        table3.classList.add('body-row-tablet-yacheika-sel')
+    });
+})
+cart3.addEventListener("mouseout", function (event) {
+    event.preventDefault();
+    tables3.forEach((table3) => {
+        table3.classList.remove('body-row-tablet-yacheika-sel')
+    });
+})
+
+cart4.addEventListener("mouseover", function (event) {
+    event.preventDefault();
+    tables4.forEach((table4) => {
+        table4.classList.add('body-row-tablet-yacheika-sel')
+    });
+})
+cart4.addEventListener("mouseout", function (event) {
+    event.preventDefault();
+    tables4.forEach((table4) => {
+        table4.classList.remove('body-row-tablet-yacheika-sel')
+    });
+})
